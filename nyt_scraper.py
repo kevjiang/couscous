618c806798f018de84db0db24b37f67aedf79f0f
Kevin Jiang <k.jiang@yale.edu>
2016-07-20 16:04:45 -0400

    updated NYT scraper and send_sms to send texts of how many times trump appears in headline of NYT

--- nyt_scraper.py
+++ nyt_scraper.py
@@ -1,24 +1,35 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-needle = "trump"
-needle_count = 0
-found_needle = False
+def nyt_headline_count(needle):
+	'''
+	Given a needle (must be lowercase!)
+	Returns number of times needle appears in 
+	headline on nytimes.com
+	'''
 
-r = urllib2.urlopen('http://www.nytimes.com/').read()
-soup = BeautifulSoup(r)
+	needle = needle.lower()
+	needle_count = 0
 
-news_item_tags = soup.find_all("h2", {"class": "story-heading"})
-news_items = []
+	r = urllib2.urlopen('http://www.nytimes.com/').read()
+	soup = BeautifulSoup(r)
 
-for elem in news_item_tags:
-	news_items.append(elem.get_text())
+	news_item_tags = soup.find_all("h2", {"class": "story-heading"})
+	news_item_tags += soup.find_all("h3", {"class": "story-heading"})
 
-print news_items
+	news_items = []
 
-for item in news_items:
-	if needle in item.lower():  # making it lower case is key!
-		found_needle = True
-		needle_count += 1
+	for elem in news_item_tags:
+		news_items.append(elem.get_text())
 
-print "How many %s's in the news today? %d" % (needle, needle_count)
+	# print news_items
+
+	for item in news_items:
+		if needle in item.lower():  # making it lower case is key!
+			needle_count += 1
+
+	# print "How many %s's in the news today? %d" % (needle, needle_count)
+
+	return needle_count
+
+	
